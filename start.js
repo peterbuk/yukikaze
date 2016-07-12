0b4d102e2703f17fefaf4cb2e7207b0ef34080e5
Peter Buk <Peter Buk>
2016-07-11 22:15:43 -0600

    wao

--- start.js
+++ start.js
@@ -9,18 +9,29 @@ beaver.on("ready", () => { // When the bot is ready
 });
 
 beaver.on("messageCreate", (msg) => {
+    /*
     if(msg.content === "!ping") {
         beaver.createMessage(msg.channel.id, "Pangcake!");
 		console.log(msg.content);
     }
+    */
+
 });
 
 beaver.connect();
 
 var timerRule = new schedule.RecurrenceRule();
+timerRule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 
 var job = schedule.scheduleJob(timerRule, function() {
-    var msg = new Date();
+    var msg = "wao";//new Date();
 
     beaver.createMessage("202272875447582729", msg);
 })
+
+/*
+* KC FUNCTIONS
+* */
+
+var pvpTimer = new schedule.RecurrenceRule();
+pvpTimer.hour = 11, 23;
