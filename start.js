101379cf877eae8c5498189699f5dbb0fe61a1c9
Peter Buk <Peter Buk>
2016-07-11 18:45:31 -0600

    .

--- start.js
+++ start.js
@@ -42,8 +42,16 @@ if (cluster.isMaster) {
 var Eris = require("eris");
 
 var beaver = new Eris("MTc1MDEyMDE3MjkwMDg0MzUy.CmW86w.sd_RFxhnTnQU7s5_Sueczz-vcgM");
-beaver.connect();
+
 
 beaver.on("ready", () => { // When the bot is ready
     console.log("Ready!"); // Log "Ready!"
 });
+
+beaver.on("messageCreate", (msg) => {
+    if(msg.content === "!ping") {
+        bot.createMessage(msg.channel.id, "Pong!");
+    }
+});
+
+beaver.connect();
