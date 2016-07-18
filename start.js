46d3afa83cbf01848537a539b369fcd8fba4570b
peterbuk8 <peterbuk8@gmail.com>
2016-07-18 13:06:35 -0600

    Fixed definition bug

--- start.js
+++ start.js
@@ -17,16 +17,7 @@ var filePath = "./db.json";
 if (process.env.OPENSHIFT_DATA_DIR != undefined)
     filePath = process.env.OPENSHIFT_DATA_DIR + "db.json";
 
-function saveDB() {
-    jsonfile.writeFileSync(filePath, db, {spaces: 2});
-}
-
-function loadDB() {
-    var db = jsonfile.readFileSync(filePath);
-    console.log(getTimestamp() + " Loaded db from " + filePath);
-}
-
-loadDB();
+var db = loadDB();
 
 // custom modules
 var kancolle = require("./kancolle.js")(beaver, db);
@@ -158,6 +149,15 @@ beaver.on("messageCreate", (msg) => {
 var dbSaveRule = new schedule.RecurrenceRule();
 dbSaveRule.minute = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 59];
 schedule.scheduleJob(dbSaveRule, saveDB);
+
+function saveDB() {
+    jsonfile.writeFileSync(filePath, db, {spaces: 2});
+}
+
+function loadDB() {
+    var db = jsonfile.readFileSync(filePath);
+    console.log(getTimestamp() + " Loaded db from " + filePath);
+}
 
 
 /*
